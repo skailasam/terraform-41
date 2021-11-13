3746ed331bcc11a0caf9432983576aeba41b22d9
Your Name <you@example.com>
2021-11-13 10:54:45 +0100

    Add files, update resources

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
-resource "local_file" "books" {
-  filename  = "books.txt"
-  content   = "My list of books"
+resource "local_file" "pets" {
+  filename        = "my_pet.txt"
+  content         = "My dog"
+  file_permission = "0600"
+}
+
+resource "random_pet" "my-pet" {
+  prefix          = "Mrs."
+  separator       = " "
+  length          = "1"
 }
